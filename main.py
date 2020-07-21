bd896c9a265233256ff335891a7f4ed72a428efe
daliborstakic <stakicdalibor64@gmail.com>
2020-07-21 09:24:47 +0200

    Added the solve function.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # The board itself, semi-changeable
 board = [
-    [0, 3, 0, 5, 0, 1, 9, 0, 6],
-    [0, 4, 7, 9, 0, 0, 0, 2, 0],
-    [6, 9, 0, 0, 4, 2, 5, 0, 3],
-    [0, 6, 0, 0, 0, 9, 0, 3, 4],
-    [0, 2, 0, 0, 0, 0, 8, 1, 0],
-    [0, 7, 3, 1, 0, 0, 6, 9, 2],
-    [4, 0, 0, 0, 2, 6, 3, 0, 0],
-    [0, 1, 0, 0, 9, 5, 2, 0, 0],
-    [0, 5, 2, 0, 0, 0, 0, 0, 0]
+    [1, 3, 0, 8, 6, 5, 0, 4, 9],
+    [0, 9, 8, 3, 7, 0, 0, 1, 0],
+    [0, 0, 4, 0, 2, 9, 0, 0, 8],
+    [3, 0, 9, 5, 0, 1, 0, 7, 6],
+    [0, 7, 0, 2, 0, 6, 0, 0, 0],
+    [2, 0, 6, 7, 4, 0, 0, 0, 1],
+    [4, 2, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 5, 0, 0, 0, 0, 0, 0],
+    [0, 8, 0, 9, 0, 0, 0, 2, 4]
 ]
 
 
@@ -35,8 +35,6 @@ def find_empty(board):
             if board[i][j] == 0:
                 return i, j  # Rows and columns
 
-    return None
-
 
 # Checks if the entered number is valid or not
 def is_valid(board, num, pos):
@@ -51,8 +49,9 @@ def is_valid(board, num, pos):
             return False
 
     # Checking 3x3 boxes
-    box_x = pos[0] // 3
-    box_y = pos[1] // 3
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
             if board[i][j] == num and (i, j) != pos:
@@ -61,4 +60,27 @@ def is_valid(board, num, pos):
     return True
 
 
+def solve(board):
+    find = find_empty(board)
+
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+print_board(board)
+solve(board)
+print("\n")
 print_board(board)
